b5968f7f1c7de004ed57001183d4b3fd4cad5b1d
prudhvi <prudhvieethakota123@gmail.com>
2021-07-04 17:33:47 +0530

    Added: Code for Checking the Employee Presence

--- src/main/kotlin/com/bridgelabz/kotlinprograms/employeewage/EmployeeWage.kt
+++ src/main/kotlin/com/bridgelabz/kotlinprograms/employeewage/EmployeeWage.kt
@@ -1,5 +1,13 @@
 package com.bridgelabz.kotlinprograms.employeewage
 
+import kotlin.math.abs
+import kotlin.random.Random
+
 fun main(args: Array<String>) {
     println("Welcome To the Employee Wage Computation In Kotlin Language")
+
+    //UC 1: Checking Employee Presence
+    val absent = 0   //Initialize the Variable
+    val empCheck = Random.nextInt(2)   //Checking with random Number
+    if (empCheck == absent) println("Employee is Absent") else println("Employee is Present")
 }
