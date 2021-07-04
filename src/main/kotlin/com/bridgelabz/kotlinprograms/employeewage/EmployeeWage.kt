cc9f9aa50928344be8af9635a02c24d950e0185f
prudhvi <prudhvieethakota123@gmail.com>
2021-07-05 00:36:35 +0530

    Added: Calculating Wages for Condition of Working Days or Hours in a Month

--- src/main/kotlin/com/bridgelabz/kotlinprograms/employeewage/EmployeeWage.kt
+++ src/main/kotlin/com/bridgelabz/kotlinprograms/employeewage/EmployeeWage.kt
@@ -8,6 +8,7 @@ const val WAGE_PER_HOUR = 20
 const val PART_TIME_HOURS = 4
 const val FULL_TIME_HOURS = 8
 const val NO_OF_WORKING_DAYS = 20
+const val MAX_HOURS_IN_MONTH = 100
 
 fun getWorkingHours(empCheck: Int): Int {
     return when (empCheck) {
@@ -21,11 +22,13 @@ fun main(args: Array<String>) {
     println("Welcome To the Employee Wage Computation In Kotlin Language")
 
     var totalEmpHours = 0
-    for(days in 1..NO_OF_WORKING_DAYS) { //Checking with random Number
-        val empCheck = Random.nextInt(3)
+    var totalWorkingDays = 0
+    while (totalEmpHours < MAX_HOURS_IN_MONTH && totalWorkingDays < NO_OF_WORKING_DAYS) {
+        totalWorkingDays++
+        val empCheck = Random.nextInt(3)   //Checking with random Number
         totalEmpHours += getWorkingHours(empCheck)
     }
 
     var employeeWage = totalEmpHours * WAGE_PER_HOUR
-    println("Hours: $totalEmpHours   Employee Wage: $employeeWage")
+    println("Total Working Days : $totalWorkingDays ,  Total Working Hours: $totalEmpHours ,  Employee Wage: $employeeWage")
 }
