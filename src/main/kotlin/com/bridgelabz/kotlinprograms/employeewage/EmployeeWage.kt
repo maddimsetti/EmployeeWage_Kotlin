709998ec31403d5f09b8fbf0c31aadaa5caf8a3f
prudhvi <prudhvieethakota123@gmail.com>
2021-07-04 23:42:32 +0530

    Added: Refactor the Code to Get Employee Hours Using Functions

--- src/main/kotlin/com/bridgelabz/kotlinprograms/employeewage/EmployeeWage.kt
+++ src/main/kotlin/com/bridgelabz/kotlinprograms/employeewage/EmployeeWage.kt
@@ -5,18 +5,27 @@ import kotlin.random.Random
 const val PARTTIME: Int = 1
 const val FULLTIME: Int = 2
 const val WAGE_PER_HOUR = 20
-fun main(args: Array<String>) {
-    println("Welcome To the Employee Wage Computation In Kotlin Language")
+const val PART_TIME_HOURS = 4
+const val FULL_TIME_HOURS = 8
 
-    //Initialize the Variable
-    var empWorkingHours = 0
-    val empCheck = Random.nextInt(3)   //Checking with random Number
+//Initialize the Variable
+var empWorkingHours = 0
+val empCheck = Random.nextInt(3)   //Checking with random Number
 
+fun getWorkingHours(): Int {
     empWorkingHours = when(empCheck) {
         PARTTIME -> 4
         FULLTIME -> 8
         else -> 0
     }
-    var dailyEmployeeWage = WAGE_PER_HOUR * empWorkingHours
-    println("Daily Employee Wage : $dailyEmployeeWage")
+    return  empWorkingHours
+}
+
+fun main(args: Array<String>) {
+    println("Welcome To the Employee Wage Computation In Kotlin Language")
+
+    var empHours = getWorkingHours()
+
+    var employeeWage = empHours * WAGE_PER_HOUR
+    println("Hours: $empHours   Employee Wage: $employeeWage")
 }
